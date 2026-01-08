e939cfca169d97eac40114a1b9f0cf5f5f212201
VitoHuang720618 <vito.huang720618@gmail.com>
2026-01-08 13:50:17 +0800

    修復 ImageButton hover 圖片位移問題：強制高度 100% 並居中對齊

--- demo/src/components/ImageButton.vue
+++ demo/src/components/ImageButton.vue
@@ -25,44 +25,14 @@ const handleClick = (event: MouseEvent) => {
 </script>
 
 <template>
-  <a
-    v-if="href"
-    :href="href"
-    :target="target"
-    :rel="target === '_blank' ? 'noopener noreferrer' : undefined"
-    class="img-button"
-    :data-index="dataIndex"
-    :data-title="dataTitle"
-    @click="handleClick"
-  >
-    <ImageComponent
-      :src="defaultSrc"
-      :alt="alt"
-      class="img-button--default"
-    />
-    <ImageComponent
-      :src="hoverSrc"
-      :alt="alt"
-      class="img-button--hover"
-    />
+  <a v-if="href" :href="href" :target="target" :rel="target === '_blank' ? 'noopener noreferrer' : undefined"
+    class="img-button" :data-index="dataIndex" :data-title="dataTitle" @click="handleClick">
+    <ImageComponent :src="defaultSrc" :alt="alt" class="img-button--default" />
+    <ImageComponent :src="hoverSrc" :alt="alt" class="img-button--hover" />
   </a>
-  <div
-    v-else
-    class="img-button"
-    :data-index="dataIndex"
-    :data-title="dataTitle"
-    @click="handleClick"
-  >
-    <ImageComponent
-      :src="defaultSrc"
-      :alt="alt"
-      class="img-button--default"
-    />
-    <ImageComponent
-      :src="hoverSrc"
-      :alt="alt"
-      class="img-button--hover"
-    />
+  <div v-else class="img-button" :data-index="dataIndex" :data-title="dataTitle" @click="handleClick">
+    <ImageComponent :src="defaultSrc" :alt="alt" class="img-button--default" />
+    <ImageComponent :src="hoverSrc" :alt="alt" class="img-button--hover" />
   </div>
 </template>
 
@@ -88,7 +58,20 @@ const handleClick = (event: MouseEvent) => {
   opacity: 0;
   position: absolute;
   top: 0;
+  width: 100%;
+  /* 確保寬度佔滿 */
+  height: 100%;
+  /* 確保高度佔滿容器，與 default 圖片一致 */
   transition: opacity 0.1s ease-in-out;
+}
+
+/* 強制 hover 圖片內容適應容器，防止溢出或偏移 */
+.img-button .img-button--hover :deep(img) {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  object-position: center;
+  /* 確保居中 */
 }
 
 .img-button:hover .img-button--default {
