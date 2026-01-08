d20933fa081e8c3de15f8de0534dc6a75aba89c3
VitoHuang720618 <vito.huang720618@gmail.com>
2026-01-08 16:00:54 +0800

    回滾 ImageButton 結構：恢復為 Default 圖片撐開 + Hover 圖片絕對定位居中的方案

--- demo/src/components/ImageButton.vue
+++ demo/src/components/ImageButton.vue
@@ -1,39 +1,31 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import ImageComponent from './ImageComponent.vue'
 
-interface ImageButtonProps {
+const props = defineProps<{
   defaultSrc: string
   hoverSrc: string
-  alt: string
+  alt?: string
   href?: string
   target?: string
-  dataIndex?: string | number
+  dataIndex?: number
   dataTitle?: string
-}
-
-withDefaults(defineProps<ImageButtonProps>(), {
-  target: '_blank'
-})
-
-const emit = defineEmits<{
-  click: [event: MouseEvent]
 }>()
 
-const handleClick = (event: MouseEvent) => {
-  emit('click', event)
+const handleClick = (e: MouseEvent) => {
+  if (!props.href) {
+    e.preventDefault()
+  }
 }
 </script>
 
 <template>
-  <a v-if="href" :href="href" :target="target" :rel="target === '_blank' ? 'noopener noreferrer' : undefined"
-    class="img-button" :data-index="dataIndex" :data-title="dataTitle" @click="handleClick">
+  <component :is="href ? 'a' : 'div'" class="img-button" :href="href" :target="href ? target : undefined"
+    :rel="href && target === '_blank' ? 'noopener noreferrer' : undefined" :data-index="dataIndex"
+    :data-title="dataTitle" @click="handleClick">
     <ImageComponent :src="defaultSrc" :alt="alt" class="img-button--default" />
     <ImageComponent :src="hoverSrc" :alt="alt" class="img-button--hover" />
-  </a>
-  <div v-else class="img-button" :data-index="dataIndex" :data-title="dataTitle" @click="handleClick">
-    <ImageComponent :src="defaultSrc" :alt="alt" class="img-button--default" />
-    <ImageComponent :src="hoverSrc" :alt="alt" class="img-button--hover" />
-  </div>
+  </component>
 </template>
 
 <style scoped>
@@ -43,7 +35,7 @@ const handleClick = (event: MouseEvent) => {
   cursor: pointer;
 }
 
-.img-button img {
+.img-button :deep(img) {
   display: block;
   width: 100%;
 }
