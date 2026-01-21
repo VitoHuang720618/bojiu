997dbae574483bb5ad30080fff728ae52c72b67d
VitoHuang720618 <vito.huang720618@gmail.com>
2026-01-21 14:43:14 +0800

    Feat: 新增本地配置同步腳本 (sync-config.js) 並更新 API 優先級邏輯

--- demo/src/config/siteConfig.ts
+++ demo/src/config/siteConfig.ts
@@ -13,42 +13,42 @@ export const siteConfig: SiteConfig = {
   title: '博九娱乐网',
   description: 'B9 Entertainment Website',
   baseUrl: getBaseUrl(),
-  useApi: true,
+  useApi: true, // Default to true, but assets below are used when API fails or is disabled
 
   navigation: [
-    {
-      "id": "btn-0",
-      "label": "寰宇瀏覽器",
-      "href": "https://www.ub66.com/",
-      "isExternal": true,
-      "default": "/defaults/button-0-defaultImage.png",
-      "hover": "/defaults/button-0-hoverImage.png"
-    },
-    {
-      "id": "btn-1",
-      "label": "APP",
-      "href": "https://haa68686.com:9900/web/simple.php#/aioDownload",
-      "isExternal": true,
-      "default": "/defaults/button-1-defaultImage.png",
-      "hover": "/defaults/button-1-hoverImage.png"
-    },
-    {
-      "id": "btn-2",
-      "label": "FUN乐园",
-      "href": "https://fun99666.com/",
-      "isExternal": true,
-      "default": "/defaults/button-2-defaultImage.png",
-      "hover": "/defaults/button-2-hoverImage.png"
-    },
-    {
-      "id": "btn-3",
-      "label": "合作夥伴",
-      "href": "https://haa68686.com:9900/web/#/article/at3",
-      "isExternal": true,
-      "default": "/defaults/button-3-defaultImage.png",
-      "hover": "/defaults/button-3-hoverImage.png"
-    }
-  ],
+  {
+    "id": "btn-0",
+    "label": "寰宇瀏覽器",
+    "href": "https://www.ub66.com/",
+    "isExternal": true,
+    "default": "/defaults/button-0-defaultImage.png",
+    "hover": "/defaults/button-0-hoverImage.png"
+  },
+  {
+    "id": "btn-1",
+    "label": "APP",
+    "href": "https://haa68686.com:9900/web/simple.php#/aioDownload",
+    "isExternal": true,
+    "default": "/defaults/button-1-defaultImage.png",
+    "hover": "/defaults/button-1-hoverImage.png"
+  },
+  {
+    "id": "btn-2",
+    "label": "FUN乐园",
+    "href": "https://fun99666.com/",
+    "isExternal": true,
+    "default": "/defaults/button-2-defaultImage.png",
+    "hover": "/defaults/button-2-hoverImage.png"
+  },
+  {
+    "id": "btn-3",
+    "label": "合作夥伴",
+    "href": "https://haa68686.com:9900/web/#/article/at3",
+    "isExternal": true,
+    "default": "/defaults/button-3-defaultImage.png",
+    "hover": "/defaults/button-3-hoverImage.png"
+  }
+],
 
   footer: {
     links: [],
@@ -86,137 +86,157 @@ export const banner = {
 
 export const backgroundImage = "/defaults/backgroundImage.png"
 
-export const routeLinksImages = Array(6).fill(null).map((_, i) => ({
-  default: `/assets/images/Lineweb-btn-${i + 1}.webp`,
-  hover: `/assets/images/Lineweb-btn-${i + 1}-hover.webp`,
-  href: ''
-}))
+export const routeLinksImages = [
+  {
+    "default": "/assets/images/Lineweb-btn-1.webp",
+    "hover": "/assets/images/Lineweb-btn-1-hover.webp",
+    "href": "http://www.yahoo.com.tw"
+  },
+  {
+    "default": "/assets/images/Lineweb-btn-2.webp",
+    "hover": "/assets/images/Lineweb-btn-2-hover.webp",
+    "href": ""
+  },
+  {
+    "default": "/assets/images/Lineweb-btn-3.webp",
+    "hover": "/assets/images/Lineweb-btn-3-hover.webp",
+    "href": ""
+  },
+  {
+    "default": "/assets/images/Lineweb-btn-4.webp",
+    "hover": "/assets/images/Lineweb-btn-4-hover.webp",
+    "href": ""
+  },
+  {
+    "default": "/assets/images/Lineweb-btn-5.webp",
+    "hover": "/assets/images/Lineweb-btn-5-hover.webp",
+    "href": ""
+  },
+  {
+    "default": "/assets/images/Lineweb-btn-6.webp",
+    "hover": "/assets/images/Lineweb-btn-6-hover.webp",
+    "href": ""
+  }
+]
 
 export const recommendedRoutes = [
   {
     "id": "route-1",
     "index": 1,
     "title": "线路 1",
-    "href": "https://www7106010814424300.asands099.com:7730/"
+    "href": "http://www.yahoo.com.tw"
   },
   {
     "id": "route-2",
     "index": 2,
     "title": "线路 2",
-    "href": "https://www7106010814424300.b02999.com:7730/"
+    "href": ""
   },
   {
     "id": "route-3",
     "index": 3,
     "title": "线路 3",
-    "href": "https://www7106010814424300.b02999.net:8866/"
+    "href": ""
   },
   {
     "id": "route-4",
     "index": 4,
     "title": "线路 4",
-    "href": "https://www7106010814424300.b6668.com:7730/"
+    "href": ""
   },
   {
     "id": "route-5",
     "index": 5,
     "title": "线路 5",
-    "href": "https://www7106010814424300.b8855b.com:9900/"
+    "href": ""
   },
   {
     "id": "route-6",
     "index": 6,
     "title": "线路 6",
-    "href": "https://www7106010814424300.b8899b.com:8866/"
+    "href": ""
   }
 ]
 
 export const recommendedTools = [
   {
-    "id": "xiaohongshu",
-    "name": "小紅書",
-    "href": "https://live.titan007.com/oldIndexall.aspx",
-    "default": "/defaults/tool-0-default.png",
-    "hover": "/defaults/tool-0-hover.png"
+    "id": "tool-0",
+    "name": "Tool 0",
+    "href": "https://www.xiaohongshu.com",
+    "default": "",
+    "hover": ""
   },
   {
-    "id": "douyin",
-    "name": "抖音",
-    "href": "https://live.leisu.com/",
-    "default": "/defaults/tool-1-default.png",
-    "hover": "/defaults/tool-1-hover.png"
+    "id": "tool-1",
+    "name": "Tool 1",
+    "href": "https://www.douyin.com",
+    "default": "",
+    "hover": ""
   },
   {
-    "id": "baidu",
-    "name": "百度",
-    "href": "https://live.nowscore.com/2in1.aspx",
-    "default": "/defaults/tool-2-default.png",
-    "hover": "/defaults/tool-2-hover.png"
-  },
-  {
-    "id": "youku",
-    "name": "YOUKU",
-    "href": "https://v.douyin.com/KZUpEUBUIlI/",
-    "default": "/defaults/tool-3-default.png",
-    "hover": "/defaults/tool-3-hover.png"
-  },
-  {
-    "id": "iqiyi",
-    "name": "iQIYI",
-    "href": "https://www.baidu.com/",
-    "default": "/defaults/tool-4-default.png",
-    "hover": "/defaults/tool-4-hover.png"
-  },
-  {
-    "id": "x",
-    "name": "X",
-    "href": "https://x.com/ezfun9527",
-    "default": "/defaults/tool-5-default.png",
-    "hover": "/defaults/tool-5-hover.png"
+    "id": "tool-2",
+    "name": "Tool 2",
+    "href": "https://www.baidu.com",
+    "default": "",
+    "hover": ""
   }
 ]
 
 export const videoThumbnails = [
   {
     "id": "video-0",
-    "title": "甜美Aura",
-    "href": "https://fun99888.com/index.php/sport-information/",
-    "image": "/defaults/videoThumbnails-0-image.png",
+    "title": "Video 0",
+    "href": "#",
+    "image": "",
     "alt": ""
   },
   {
     "id": "video-1",
-    "title": "变态马洛",
-    "href": "https://fun99888.com/index.php/sport-information/",
-    "image": "/defaults/videoThumbnails-1-image.png",
+    "title": "Video 1",
+    "href": "#",
+    "image": "",
     "alt": ""
   },
   {
     "id": "video-2",
-    "title": "蹦蹦冲啊",
-    "href": "https://fun99888.com/index.php/sport-information/",
-    "image": "/assets/images/c370f50d-c248-4167-927f-c9af0bae3351.png",
+    "title": "Video 2",
+    "href": "#",
+    "image": "",
     "alt": ""
   },
   {
     "id": "video-3",
-    "title": "阳光Hank",
-    "href": "https://fun99888.com/index.php/sport-information/",
-    "image": "/assets/images/612623c8-aa39-44be-8eec-6d57d645bcfb.png",
+    "title": "Video 3",
+    "href": "#",
+    "image": "",
     "alt": ""
   },
   {
     "id": "video-4",
-    "title": "专属福利",
-    "href": "https://example.com/video5",
-    "image": "/assets/images/4e67c664-2a43-4ac2-9e09-dff556a007ce.png",
+    "title": "Video 4",
+    "href": "#",
+    "image": "",
     "alt": ""
   },
   {
     "id": "video-5",
-    "title": "撩人双飞闺蜜秀",
-    "href": "https://fun99888.com/index.php/sport-information/",
-    "image": "/assets/images/af698bb9-9183-4714-b568-fa7ef0721cfc.png",
+    "title": "Video 5",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "video-6",
+    "title": "Video 6",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "video-7",
+    "title": "Video 7",
+    "href": "#",
+    "image": "",
     "alt": ""
   }
 ]
@@ -224,45 +244,115 @@ export const videoThumbnails = [
 export const programThumbnails = [
   {
     "id": "program-0",
-    "title": "Fun乐园圣典直播",
-    "href": "https://www.fun99666.com/index.php/funfans/",
-    "image": "/defaults/programThumbnails-0-image.png",
+    "title": "Program 0",
+    "href": "#",
+    "image": "",
     "alt": ""
   },
   {
     "id": "program-1",
-    "title": "熱門節目 2",
-    "href": "https://example.com/program2",
-    "image": "/assets/images/c67bb6cc-e00c-4811-a62a-38604bda41f7.png",
-    "alt": "火熱節目 2"
+    "title": "Program 1",
+    "href": "#",
+    "image": "",
+    "alt": ""
   },
   {
     "id": "program-2",
-    "title": "熱門節目 3",
-    "href": "https://example.com/program3",
-    "image": "/assets/images/391bcfda-fff2-4a55-b3d2-444884ddf87f.png",
-    "alt": "火熱節目 3"
+    "title": "Program 2",
+    "href": "#",
+    "image": "",
+    "alt": ""
   },
   {
     "id": "program-3",
-    "title": "熱門節目 4",
-    "href": "https://example.com/program4",
-    "image": "/assets/images/a1d2e390-0a0f-4dde-8d9b-6de5bb9f4de0.jpg",
-    "alt": "火熱節目 4"
+    "title": "Program 3",
+    "href": "#",
+    "image": "",
+    "alt": ""
   },
   {
     "id": "program-4",
-    "title": "熱門節目 5",
-    "href": "https://example.com/program5",
-    "image": "/assets/images/626e1c19-bf48-40ed-9a2f-8e2a8e4dce13.png",
-    "alt": "火熱節目 5"
+    "title": "Program 4",
+    "href": "#",
+    "image": "",
+    "alt": ""
   },
   {
     "id": "program-5",
-    "title": "熱門節目 6",
-    "href": "https://example.com/program6",
-    "image": "/assets/images/95e0ab84-342d-4bf0-8cbc-3b6e107308f3.jpg",
-    "alt": "火熱節目 6"
+    "title": "Program 5",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-6",
+    "title": "Program 6",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-7",
+    "title": "Program 7",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-8",
+    "title": "Program 8",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-9",
+    "title": "Program 9",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-10",
+    "title": "Program 10",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-11",
+    "title": "Program 11",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-12",
+    "title": "Program 12",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-13",
+    "title": "Program 13",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-14",
+    "title": "Program 14",
+    "href": "#",
+    "image": "",
+    "alt": ""
+  },
+  {
+    "id": "program-15",
+    "title": "Program 15",
+    "href": "#",
+    "image": "",
+    "alt": ""
   }
 ]
 
@@ -271,31 +361,37 @@ export const carouselSlides = [
     "id": "slide-0",
     "image": "/defaults/carousel-0.png",
     "href": "",
-    "alt": "輪播圖 1"
+    "alt": "Carousel 0"
+  },
+  {
+    "id": "slide-1",
+    "image": "/defaults/carousel-1.png",
+    "href": "",
+    "alt": "Carousel 1"
   }
 ]
 
 export const floatAdButtons = [
   {
-    "id": "customer-service",
-    "name": "在线客服",
-    "href": "https://3pisx.60bfjtkb.com/5d54e83dd5f4de784021jkfle-kelid25743e4810614cc6843da934461925da950daa8a0a72e5a809b5d75b381e777",
-    "default": "/defaults/float-0-default.png",
-    "hover": "/defaults/float-0-hover.png"
+    "id": "float-0",
+    "name": "Float 0",
+    "href": "https://example.com/customer-service",
+    "default": "",
+    "hover": ""
   },
   {
-    "id": "girl-douyin",
-    "name": "女孩抖音",
-    "href": "https://v.douyin.com/KZUpEUBUIlI/",
-    "default": "/defaults/float-1-default.png",
-    "hover": "/defaults/float-1-hover.png"
+    "id": "float-1",
+    "name": "Float 1",
+    "href": "https://example.com/girl-douyin",
+    "default": "",
+    "hover": ""
   },
   {
-    "id": "sports-douyin",
-    "name": "体育抖音",
-    "href": "https://www.fun99666.com/",
-    "default": "/defaults/float-2-default.png",
-    "hover": "/defaults/float-2-hover.png"
+    "id": "float-2",
+    "name": "Float 2",
+    "href": "https://example.com/sports-douyin",
+    "default": "",
+    "hover": ""
   }
 ]
 
---
+++ scripts/sync-config.js
@@ -0,0 +1,213 @@
+const fs = require('fs');
+const path = require('path');
+
+// Paths
+const ROOT_DIR = path.resolve(__dirname, '..');
+const CONFIG_JSON_PATH = path.join(ROOT_DIR, 'manager/backend/data/config.json');
+const SOURCE_UPLOADS_DIR = path.join(ROOT_DIR, 'manager/backend/uploads');
+const TARGET_DEFAULTS_DIR = path.join(ROOT_DIR, 'demo/public/defaults');
+const TARGET_SITE_CONFIG_PATH = path.join(ROOT_DIR, 'demo/src/config/siteConfig.ts');
+
+// Ensure target defaults directory exists
+if (!fs.existsSync(TARGET_DEFAULTS_DIR)) {
+    fs.mkdirSync(TARGET_DEFAULTS_DIR, { recursive: true });
+}
+
+// Read config.json
+let config = {};
+try {
+    const data = fs.readFileSync(CONFIG_JSON_PATH, 'utf8');
+    config = JSON.parse(data);
+    console.log('✅ Loaded config.json');
+} catch (err) {
+    console.error('❌ Failed to read config.json:', err);
+    process.exit(1);
+}
+
+// Helper to copy image and return new path
+function processImage(url) {
+    if (!url || !url.includes('/uploads/')) return '';
+
+    const filename = url.split('/uploads/').pop();
+    const sourcePath = path.join(SOURCE_UPLOADS_DIR, filename);
+    const targetPath = path.join(TARGET_DEFAULTS_DIR, filename);
+
+    if (fs.existsSync(sourcePath)) {
+        fs.copyFileSync(sourcePath, targetPath);
+        // console.log(`Copied: ${filename}`);
+    } else {
+        console.warn(`⚠️ Warning: Image source not found: ${sourcePath}`);
+    }
+
+    return `/defaults/${filename}`;
+}
+
+// --- Process Data ---
+
+// 1. Banner
+const banner = {
+    pc: processImage(config.banner.pc),
+    tablet: processImage(config.banner.tablet),
+    mobile: processImage(config.banner.mobile)
+};
+
+// 2. Background
+const backgroundImage = processImage(config.backgroundImage);
+
+// 3. Navigation (Button Links)
+const navigation = (config.buttonLinks || []).map((btn, index) => ({
+    id: `btn-${index}`,
+    label: btn.text || '',
+    href: btn.href || '#',
+    isExternal: btn.target === '_blank',
+    default: processImage(btn.defaultImage),
+    hover: processImage(btn.hoverImage)
+}));
+
+// 4. Route Links
+// Split into images and data
+const routeLinksImages = (config.routeLinks || []).map((link, index) => ({
+    default: processImage(link.default) || `/assets/images/Lineweb-btn-${index + 1}.webp`, // Fallback to asset if empty? Or keep empty? Keeping empty might break layout if not careful, but manager allows empty. Let's use what's in config.
+    hover: processImage(link.hover) || `/assets/images/Lineweb-btn-${index + 1}-hover.webp`,
+    href: link.href || ''
+}));
+
+// We need to reconstruct recommendedRoutes. `config.json` lacks titles, so we use generic ones or keep existing logic? 
+// The implementation plan implies using manager data as default.
+const recommendedRoutes = (config.routeLinks || []).map((link, index) => ({
+    id: `route-${index + 1}`,
+    index: index + 1,
+    title: `线路 ${index + 1}`, // Generic title since JSON doesn't have it
+    href: link.href || ''
+}));
+
+
+// 5. Tool Icons (Recommended Tools)
+const recommendedTools = (config.toolIcons || []).map((tool, index) => ({
+    id: `tool-${index}`,
+    name: `Tool ${index}`, // Manager JSON doesn't seem to have name/title for tools? Wait, let's check config.json again.
+    // Viewing config.json: It has href, default, hover. NO name/title.
+    // siteConfig.ts has name: '小紅書' etc.
+    // Using generic names if key is missing.
+    href: tool.href || '#',
+    default: processImage(tool.default),
+    hover: processImage(tool.hover)
+}));
+// Fix: Attempt to map known domains to names? Or just use generic? 
+// User request is to use Manager data. If Manager doesn't have names, default shouldn't have specific names potentially.
+// But let's check if I can keep existing names if URL matches? Too complex.
+// Let's stick to generic or empty.
+
+
+// 6. Video Thumbnails
+const videoThumbnails = (config.videoThumbnails || []).map((video, index) => ({
+    id: `video-${index}`,
+    title: video.title || `Video ${index}`,
+    href: video.href || '#',
+    image: processImage(video.image),
+    alt: video.alt || ''
+}));
+
+// 7. Program Thumbnails
+const programThumbnails = (config.programThumbnails || []).map((program, index) => ({
+    id: `program-${index}`,
+    title: program.title || `Program ${index}`,
+    href: program.href || '#',
+    image: processImage(program.image),
+    alt: program.alt || ''
+}));
+
+// 8. Carousel Slides
+const carouselSlides = (config.carouselSlides || []).map((slide, index) => ({
+    id: `slide-${index}`,
+    image: processImage(slide.image),
+    href: slide.href || '',
+    alt: slide.title || `Carousel ${index}`
+}));
+
+// 9. Float Ad Buttons
+const floatAdButtons = (config.floatAdButtons || []).map((btn, index) => ({
+    id: `float-${index}`,
+    name: `Float ${index}`,
+    href: btn.href || '#',
+    default: processImage(btn.default),
+    hover: processImage(btn.hover)
+}));
+
+
+// --- Generate File Content ---
+
+const fileContent = `import type { SiteConfig } from '../types'
+
+// Get base URL from environment or use relative path for container deployment
+const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    return window.location.origin
+  }
+  return import.meta.env.VITE_BASE_URL || ''
+}
+
+// Site configuration (Migrated from Manager)
+export const siteConfig: SiteConfig = {
+  title: '博九娱乐网',
+  description: 'B9 Entertainment Website',
+  baseUrl: getBaseUrl(),
+  useApi: true, // Default to true, but assets below are used when API fails or is disabled
+
+  navigation: ${JSON.stringify(navigation, null, 2)},
+
+  footer: {
+    links: [],
+    socialMedia: [],
+    copyright: 'Copyright © 博九娱乐网 Reserved'
+  },
+
+  theme: {
+    colors: {
+      primary: '#ba081f',
+      secondary: '#8b0012',
+      background: '#16181b',
+      text: '#ffd08c',
+      accent: '#dfb082'
+    },
+    fonts: {
+      heading: 'Arial, Microsoft Yahei, PingFangSC, sans-serif',
+      body: 'Arial, Microsoft Yahei, PingFangSC, sans-serif'
+    },
+    breakpoints: {
+      mobile: 480,
+      tablet: 768,
+      desktop: 1024
+    }
+  }
+}
+
+// --- Migrated Assets & Content ---
+
+export const banner = ${JSON.stringify(banner, null, 2)}
+
+export const backgroundImage = "${backgroundImage}"
+
+export const routeLinksImages = ${JSON.stringify(routeLinksImages, null, 2)}
+
+export const recommendedRoutes = ${JSON.stringify(recommendedRoutes, null, 2)}
+
+export const recommendedTools = ${JSON.stringify(recommendedTools, null, 2)}
+
+export const videoThumbnails = ${JSON.stringify(videoThumbnails, null, 2)}
+
+export const programThumbnails = ${JSON.stringify(programThumbnails, null, 2)}
+
+export const carouselSlides = ${JSON.stringify(carouselSlides, null, 2)}
+
+export const floatAdButtons = ${JSON.stringify(floatAdButtons, null, 2)}
+
+export const videoContent = videoThumbnails
+export const programContent = programThumbnails
+export const buttonLinks = siteConfig.navigation
+`;
+
+// Write file
+fs.writeFileSync(TARGET_SITE_CONFIG_PATH, fileContent, 'utf8');
+console.log('✅ Generated siteConfig.ts');
+console.log('🎉 Done! Manager config synced to Demo defaults.');
